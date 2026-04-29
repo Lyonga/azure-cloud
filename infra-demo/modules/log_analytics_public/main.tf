a85ca4f9baf5c6ec2845f006bd5d3c48b44f0b48
Lyonga <60431189+Lyonga@users.noreply.github.com>
2026-04-29 16:24:15 -0400

    revert

--- infra-demo/modules/log_analytics_public/main.tf
+++ infra-demo/modules/log_analytics_public/main.tf
@@ -1,27 +1,27 @@
-# terraform {
-#   required_providers {
-#     azurerm = {
-#       source  = "hashicorp/azurerm"
-#       version = "~> 3.0"
-#     }
-#   }
-# }
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
 
-# module "log_analytics" {
-#   source  = "Umanis/log-analytics-workspace/azurerm"
-#   version = "1.0.0"
-#   # source  = "terraform-az-modules/log-analytics/azurerm"
-#   # version = "1.0.3"
+module "log_analytics" {
+  source  = "Umanis/log-analytics-workspace/azurerm"
+  version = "1.0.0"
+  # source  = "terraform-az-modules/log-analytics/azurerm"
+  # version = "1.0.3"
 
-#   resource_group_name        = var.resource_group_name
-#   instance_index             = var.instance_index
-#   custom_name                = var.custom_name
-#   custom_location            = var.location
-#   retention_in_days          = var.retention_in_days
-#   daily_quota_gb             = var.daily_quota_gb
-#   internet_ingestion_enabled = var.internet_ingestion_enabled
-#   internet_query_enabled     = var.internet_query_enabled
-#   subscription_id            = var.subscription_id
-#   tenant_id                  = var.tenant_id
-#   custom_tags                = var.tags
-# }
+  resource_group_name        = var.resource_group_name
+  instance_index             = var.instance_index
+  custom_name                = var.custom_name
+  custom_location            = var.location
+  retention_in_days          = var.retention_in_days
+  daily_quota_gb             = var.daily_quota_gb
+  internet_ingestion_enabled = var.internet_ingestion_enabled
+  internet_query_enabled     = var.internet_query_enabled
+  subscription_id            = var.subscription_id
+  tenant_id                  = var.tenant_id
+  custom_tags                = var.tags
+}
