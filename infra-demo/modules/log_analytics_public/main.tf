28f1ffb596f47d9312ed12bab86dbb20bab56eb3
Lyonga <60431189+Lyonga@users.noreply.github.com>
2026-04-29 16:00:06 -0400

    newwwwww

--- infra-demo/modules/log_analytics_public/main.tf
+++ infra-demo/modules/log_analytics_public/main.tf
@@ -9,7 +9,7 @@ terraform {
 
 module "log_analytics" {
   source  = "Umanis/log-analytics-workspace/azurerm"
-  version = "2.0.0"
+  version = "1.0.0"
   # source  = "terraform-az-modules/log-analytics/azurerm"
   # version = "1.0.3"
 
