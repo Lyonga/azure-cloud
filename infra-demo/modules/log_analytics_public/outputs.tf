adb3e1bc099257f79c8a5f8cfb7c52bc364da5ef
Lyonga <60431189+Lyonga@users.noreply.github.com>
2026-04-30 07:40:42 -0400

    put back

--- infra-demo/modules/log_analytics_public/outputs.tf
+++ infra-demo/modules/log_analytics_public/outputs.tf
@@ -1,14 +1,14 @@
-# output "workspace_name" {
-#   description = "The name of the Log Analytics workspace."
-#   value       = module.log_analytics.name
-# }
+output "workspace_name" {
+  description = "The name of the Log Analytics workspace."
+  value       = module.log_analytics.name
+}
 
-# output "workspace_id" {
-#   description = "The ID of the Log Analytics workspace."
-#   value       = module.log_analytics.id
-# }
+output "workspace_id" {
+  description = "The ID of the Log Analytics workspace."
+  value       = module.log_analytics.id
+}
 
-# output "workspace_customer_id" {
-#   description = "The customer ID of the Log Analytics workspace."
-#   value       = module.log_analytics.workspace_id
-# }
+output "workspace_customer_id" {
+  description = "The customer ID of the Log Analytics workspace."
+  value       = module.log_analytics.workspace_id
+}
--- infra-demo/modules/log_analytics_public/variables.tf
+++ infra-demo/modules/log_analytics_public/variables.tf
@@ -1,54 +1,54 @@
-# variable "custom_name" {
-#   description = "Custom name for the Log Analytics workspace."
-#   type        = string
-# }
+variable "custom_name" {
+  description = "Custom name for the Log Analytics workspace."
+  type        = string
+}
 
-# variable "resource_group_name" {
-#   description = "Name of the resource group."
-#   type        = string
-# }
+variable "resource_group_name" {
+  description = "Name of the resource group."
+  type        = string
+}
 
-# variable "resource_group_id" {
-#   description = "Resource group ID used to sequence the legacy public module when the group is created in the same plan."
-#   type        = string
-# }
+variable "resource_group_id" {
+  description = "Resource group ID used to sequence the legacy public module when the group is created in the same plan."
+  type        = string
+}
 
-# variable "location" {
-#   description = "Azure region."
-#   type        = string
-# }
+variable "location" {
+  description = "Azure region."
+  type        = string
+}
 
-# variable "instance_index" {
-#   description = "Instance index required by the public module naming logic."
-#   type        = number
-# }
+variable "instance_index" {
+  description = "Instance index required by the public module naming logic."
+  type        = number
+}
 
-# variable "retention_in_days" {
-#   description = "Workspace data retention in days."
-#   type        = number
-# }
+variable "retention_in_days" {
+  description = "Workspace data retention in days."
+  type        = number
+}
 
-# variable "daily_quota_gb" {
-#   description = "Daily ingestion quota in GB. Use -1 for unlimited ingestion."
-#   type        = number
-# }
+variable "daily_quota_gb" {
+  description = "Daily ingestion quota in GB. Use -1 for unlimited ingestion."
+  type        = number
+}
 
-# variable "internet_ingestion_enabled" {
-#   description = "Whether internet ingestion is enabled."
-#   type        = bool
-# }
+variable "internet_ingestion_enabled" {
+  description = "Whether internet ingestion is enabled."
+  type        = bool
+}
 
-# variable "internet_query_enabled" {
-#   description = "Whether internet query is enabled."
-#   type        = bool
-# }
+variable "internet_query_enabled" {
+  description = "Whether internet query is enabled."
+  type        = bool
+}
 
-# variable "subscription_id" {
-#   description = "Azure subscription ID passed to the public module."
-#   type        = string
-# }
+variable "subscription_id" {
+  description = "Azure subscription ID passed to the public module."
+  type        = string
+}
 
-# variable "tags" {
-#   description = "Tags to apply to resources."
-#   type        = map(string)
-# }
+variable "tags" {
+  description = "Tags to apply to resources."
+  type        = map(string)
+}
